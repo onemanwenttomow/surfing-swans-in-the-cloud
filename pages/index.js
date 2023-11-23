2ba5de9e3f54b79695bbb257f3e982d8b02a9e3e
Peter Anderson <pete.anderson@gmail.com>
2023-11-23 10:10:32 +0100

    Add basic image upload

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
 export default function HomePage() {
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    const response = await fetch("/api/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    console.log("data: ", data);
+  }
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="file" name="file" accept="image/*" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
